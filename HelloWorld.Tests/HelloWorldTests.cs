63f79a2aeb410c80ceeb06ed83a7c5b0a431a592
A-Guldborg <95026056+A-Guldborg@users.noreply.github.com>
2022-09-03 14:27:59 +0200

    Tested and Implemented IsLeapYear
    * Initial commit
    
    * Test fail
    
    * Test should now fail
    
    * Added --- to yaml
    
    * Added underscore to workflow_dispatch
    
    * Fixed ! in Hello, World! so now it should work!
    
    * Leap year tests but leap year is always false
    
    * Leap year divisible by 4 implemented
    
    * Centennial not leap year test
    
    * Centennial not leap year implemented unless divisible by 400

--- HelloWorld.Tests/HelloWorldTests.cs
+++ HelloWorld.Tests/HelloWorldTests.cs
@@ -3,17 +3,56 @@ namespace HelloWorld.Tests;
 public class HelloWorldTests
 {
     [Fact]
-    public void Test1()
+    public void Hello_World_Test()
     {
         // Arrange 
         using var writer = new StringWriter();
         Console.SetOut(writer);
 
         // Act
-        HelloWorld.Main(null);
+        HelloWorld.Main(Array.Empty<string>());
 
         // Assert
         var output = writer.GetStringBuilder().ToString().TrimEnd();
         output.Should().Be("Hello, World!");
+    }
+
+    [Fact]
+    public void Leap_Year_2004_When_Divisible_By_4() {
+        // Arrange
+        var year = 2004;
+        var obj = new HelloWorld();
+
+        // Act
+        var isLeapYear = obj.IsLeapYear(year);
+
+        // Assert
+        isLeapYear.Should().Be(true);
+    }
+
+    [Fact]
+    public void Not_Leap_Year_1999_When_Not_Divisible_By_4() {
+        // Arrange
+        var year = 1999;
+        var obj = new HelloWorld();
+
+        // Act
+        var isLeapYear = obj.IsLeapYear(year);
+
+        // Assert
+        isLeapYear.Should().Be(false);
+    }
+
+    [Fact]
+    public void Centennial_Not_Divisible_By_400_Is_Not_Leap_Year() {
+        // Arrange
+        var year = 1900;
+        var obj = new HelloWorld();
+
+        // Act
+        var isLeapYear = obj.IsLeapYear(year);
+
+        // Assert
+        isLeapYear.Should().Be(false);
     }
 }
--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,4 +4,11 @@ public class HelloWorld {
     public static void Main(string[] args) {
         Console.WriteLine("Hello, World!");
     }
+    
+    public bool IsLeapYear(int year) {
+        if (year % 100 == 0) {
+            return (year % 400 == 0);
+        }
+        return (year % 4) == 0;
+    }
 }
