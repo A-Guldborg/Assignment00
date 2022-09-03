676b3dbdd3fd2056880dbb7f028f664d0f7a8794
A-Guldborg <95026056+A-Guldborg@users.noreply.github.com>
2022-09-03 15:23:32 +0200

    Command Line Interface for LeapYear (#5)
    * CLI tests implemented
    
    * CLI implemented

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -2,10 +2,22 @@
 
 public class HelloWorld {
     public static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please enter year and continue with [Enter]");
+        CommandLineLeapYear(Console.ReadLine());
+    }
+
+    public static void CommandLineLeapYear(string input) {
+        int year = Int32.Parse(input);
+
+        Boolean isLeapYear = IsLeapYear(year);
+        if (isLeapYear) {
+            Console.WriteLine("yay");
+        } else {
+            Console.WriteLine("nay");
+        }    
     }
     
-    public bool IsLeapYear(int year) {
+    public static bool IsLeapYear(int year) {
         if (year % 100 == 0) {
             return (year % 400 == 0);
         }
