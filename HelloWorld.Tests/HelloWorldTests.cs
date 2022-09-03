05ce0f79018d903dc37372d9e06d2bc4a570b7b9
A-Guldborg <aguh@itu.dk>
2022-09-03 15:20:12 +0200

    CLI tests implemented

--- HelloWorld.Tests/HelloWorldTests.cs
+++ HelloWorld.Tests/HelloWorldTests.cs
@@ -3,28 +3,43 @@ namespace HelloWorld.Tests;
 public class HelloWorldTests
 {
     [Fact]
-    public void Hello_World_Test()
+    public void CLI_1996_Is_Leap_Year()
     {
         // Arrange 
         using var writer = new StringWriter();
         Console.SetOut(writer);
 
         // Act
-        HelloWorld.Main(Array.Empty<string>());
+        HelloWorld.CommandLineLeapYear("1996");
 
         // Assert
         var output = writer.GetStringBuilder().ToString().TrimEnd();
-        output.Should().Be("Hello, World!");
+        output.Should().Be("yay");
     }
+
+    [Fact]
+    public void CLI_1997_Is_Not_Leap_Year()
+    {
+        // Arrange 
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        // Act
+        HelloWorld.CommandLineLeapYear("1997");
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("nay");
+    }
+
 
     [Fact]
     public void Leap_Year_2004_When_Divisible_By_4() {
         // Arrange
         var year = 2004;
-        var obj = new HelloWorld();
 
         // Act
-        var isLeapYear = obj.IsLeapYear(year);
+        var isLeapYear = HelloWorld.IsLeapYear(year);
 
         // Assert
         isLeapYear.Should().Be(true);
@@ -34,10 +49,9 @@ public class HelloWorldTests
     public void Not_Leap_Year_1999_When_Not_Divisible_By_4() {
         // Arrange
         var year = 1999;
-        var obj = new HelloWorld();
 
         // Act
-        var isLeapYear = obj.IsLeapYear(year);
+        var isLeapYear = HelloWorld.IsLeapYear(year);
 
         // Assert
         isLeapYear.Should().Be(false);
@@ -47,10 +61,9 @@ public class HelloWorldTests
     public void Centennial_Not_Divisible_By_400_Is_Not_Leap_Year() {
         // Arrange
         var year = 1900;
-        var obj = new HelloWorld();
 
         // Act
-        var isLeapYear = obj.IsLeapYear(year);
+        var isLeapYear = HelloWorld.IsLeapYear(year);
 
         // Assert
         isLeapYear.Should().Be(false);
--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -2,10 +2,15 @@
 
 public class HelloWorld {
     public static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please enter year and continue with [Enter]");
+        CommandLineLeapYear(Console.ReadLine());
+    }
+
+    public static void CommandLineLeapYear(string input) {
+        throw new NotImplementedException();
     }
     
-    public bool IsLeapYear(int year) {
+    public static bool IsLeapYear(int year) {
         if (year % 100 == 0) {
             return (year % 400 == 0);
         }
